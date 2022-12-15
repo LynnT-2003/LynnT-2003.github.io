bb815cac9d62481bf6b15f1aeea2469c9ced28f7
Lynn Thit Nyi Nyi <lnyinyi22@gmail.com>
2022-12-15 12:47:08 +0700

    fixed bugs and cleaned code

--- cart.js
+++ cart.js
@@ -80,62 +80,6 @@ function loadData() {
 
 }
 
-function loadDataOld() {
-    var e = document.getElementById("myName");
-    e.innerHTML = "Lynn Thit Nyi Nyi"
-
-    // first get table, productList
-    var productList = document.getElementById("productList")
-
-
-    // inside tr we need td, so we create new var data
-    var row = document.createElement("tr")
-    var data = document.createElement("td")
-    data.innerHTML = "Product 1"
-    
-    var sum = 0
-    for (let p in products) {
-
-        // we need td inside tr
-        var row = document.createElement("tr")
-        var productName = document.createElement("td")
-        productName.innerHTML = products[p].name
-
-        let quantity = document.createElement("td")
-        quantity.innerHTML = products[p].Quantity
-        quantity.classList.add("text_center")
-        
-        let ppu = document.createElement("td")
-        ppu.innerHTML=products[p].PPU
-        ppu.classList.add("text_right")
-        
-        let total = document.createElement("td")
-        total.innerHTML=products[p].Quantity * products[p].PPU
-        total.classList.add("text_right")
-
-        sum += products[p].Quantity * products[p].PPU
-        vat = 0.07 * sum
-        net = sum + vat
-
-        
-        // append data to row, append row to productList
-        row.appendChild(productName)
-        row.appendChild(quantity)
-        row.appendChild(ppu)
-        row.appendChild(total)
-        productList.appendChild(row)
-    }
-
-    let grossElem = document.getElementById("grossID")
-    grossElem.innerHTML = sum
-
-    let vatElem = document.getElementById("vatID")
-    vatElem.innerHTML = Math.ceil(vat)-1
-
-    let netElem = document.getElementById("netID")
-    netElem.innerHTML = net
-}
-
 // TODO Should use product ID instead of name
 function deleteProduct(index) {
     console.log("DELETE",index)
