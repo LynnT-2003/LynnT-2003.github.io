7eefe9bab8e9711fa1a9b0c99435fe90449fa251
Lynn Thit Nyi Nyi <lnyinyi22@gmail.com>
2022-12-24 01:15:22 +0700

    Created 'Add' using Modal Form

--- Customers/customers.js
+++ Customers/customers.js
@@ -1,4 +1,42 @@
-var customers=[]
+var customers=[
+    {
+        name: "Leon Kennedy",
+        email: "leon@racoon.com",
+        phone: "098-123-4567"
+    },
+    {
+        name: "Clair Redfield",
+        email: "clair@racoon.com",
+        phone: "098-123-9999"
+    },
+    {
+        name: "Jill Valentine",
+        email: "jill@racoon.com",
+        phone: "096-456-9999"
+    },
+]
+
+function addCustomer() {
+
+    let elName = document.getElementById("inputName")
+    let elEmail = document.getElementById("inputEmail")
+    let elPhone = document.getElementById("inputPhone")
+    let customerObject = {
+        addedName: elName.value,
+        addedEmail: $('#inputEmail').val(),
+        addedPhone: $('#inputPhone').val(),
+    }
+    
+    customers.push(customerObject)
+    console.log(customers)
+    let cStr = `<tr>
+        <td>${customerObject.addedName}</td>
+        <td>${customerObject.addedEmail}</td>
+        <td>${customerObject.addedPhone}</td>
+    <tr>`
+    // $('#productBody').html(cStr) // this also works
+    $("#data-table tr:last").after(cStr)
+}
 
 $(document).ready(function () {
     console.log("ready!");
@@ -8,6 +46,7 @@ $(document).ready(function () {
     }).done(function (data) {
         //$(this).addClass("done");
         console.log("DONE",data)
+        
         for(let d in data){
             let dataStr = `<tr>
                 <td>${data[d].name}</td>
